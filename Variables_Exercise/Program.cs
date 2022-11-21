89b9fa7e81e7382ffd91f9ff7d6d40ef65895ae4
Nick Carlson <CarlsonNicholas82@gmail.com>
2022-11-21 11:09:15 -0500

    Inital Commit

---
+++ Variables_Exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿//Nick Carlson Assignment
+// Step 1
+
+string Hello = "Hello!";
+Console.WriteLine(Hello);
+
+int age = 22;
+Console.WriteLine($"You are {age} years old");
+
+char Letter = 'N';
+Console.WriteLine(Letter);
+
+bool Sunny_Day = true;
+Console.WriteLine(Sunny_Day);
+
+double num = 120.909;
+Console.WriteLine(num);
+
+decimal pi = 3.14159m;
+Console.WriteLine(pi);
+
+// Step 2
+
+Console.WriteLine($"The first letter of my name is {Letter}, I am {age} year's old!");
+
